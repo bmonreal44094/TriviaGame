483f09fde85c5c252507c5b2d9b3d5346f75e6a0
Bill Monreal <BMonreal@mcc.local>
2017-08-14 23:58:24 -0400

    final copy but I need to add var definitions for the functions that sit at the root as per Justin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,91 +7,91 @@ var intervalId;
 var clockRunning = false;
 var questions = [{
 		id: 0,
-		question: "test",
-		answer1: "answer1",
-		answer2: "answer2",
-		answer3: "answer3",
-		answer4: "answer4",
+		question: "What is the circumference of the earth in miles?",
+		answer1: "4,872",
+		answer2: "21,303",
+		answer3: "24,901",
+		answer4: "2,853",
 		correctAnswer: 3,
-		correctAnswerNarative: "blah blah blah blah",
-		answerImage: "assets/images/luke.jfif",
+		correctAnswerNarative: "The circumference of the earth in miles is 24,901.",
+		answerImage: "assets/images/earth.jfif",
 		},
 		{
 		id: 1,
-		question: "test 2",
-		answer1: "answer1",
-		answer2: "answer2",
-		answer3: "answer3",
-		answer4: "answer4",
-		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		question: "How old do scientist believe the earth is?",
+		answer1: "4.6 Billion Years",
+		answer2: "2.6 Million Years",
+		answer3: "5,000 Years",
+		answer4: "2.6 Billion Years",
+		correctAnswer: 1,
+		correctAnswerNarative: "The earth is believed to be 4.6 Billion Years old!",
+		answerImage: "assets/images/earth.jfif",
 		},
 		{
 		id: 2,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
+		question: "The observable universe is estimated to contain how many galaxies:",
+		answer1: "6 to 18 Trillion",
+		answer2: "10 to 20 Million",
+		answer3: "200 Billion to 1 Trillion",
+		answer4: "7,000 to 45,000",
 		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		correctAnswerNarative: "The observable universe is estimated to contain 200 Billion to 1 Trillion galaxies!",
+		answerImage: "assets/images/cosmicWeb.jpg",
 		},
 		{
 		id: 3,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
-		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		question: "The Milky Way contains how many stars?",
+		answer1: "200,000 to 560,000",
+		answer2: "200 to 400 Billion",
+		answer3: "6,000 to 16,000",
+		answer4: "1 to 3 Trillion",
+		correctAnswer: 2,
+		correctAnswerNarative: "The Milky Way contains 200 to 400 Billion stars!",
+		answerImage: "assets/images/via_lactea0.jpg",
 		},
 		{
 		id: 4,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
-		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		question: "The speed of light is?",
+		answer1: "299,792,458 meters/second",
+		answer2: "299,792,458 feet/second",
+		answer3: "299,792,458 miles/day",
+		answer4: "299,792,458 inches/year",
+		correctAnswer: 1,
+		correctAnswerNarative: "The speed of light is 299,792,458 meters/second",
+		answerImage: "assets/images/faster-than-the-speed-of-light.jpg",
 		},
 		{
 		id: 5,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
-		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		question: "The distance from earth to moon is?",
+		answer1: "1 Million Miles",
+		answer2: "3,945 Miles",
+		answer3: "452,900 Miles",
+		answer4: "238,900 Miles",
+		correctAnswer: 4,
+		correctAnswerNarative: "The distance from earth to moon is 238,900 Miles",
+		answerImage: "assets/images/earth_and_moon.png",
 		},
 		{
 		id: 6,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
+		question: "The circumference of the moon is?",
+		answer1: "6,786 Miles",
+		answer2: "2,399 Miles",
+		answer3: "20,150 Miles",
+		answer4: "46 Miles",
 		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		correctAnswerNarative: "The circumference of the moon is 6,786 Miles",
+		answerImage: "assets/images/our_closest_mainimage.jpg",
 		},
 		{
 		id: 7,
-		question: "",
-		answer1: "",
-		answer2: "",
-		answer3: "",
-		answer4: "",
-		correctAnswer: 3,
-		correctAnswerNarative: "",
-		answerImage: "",
+		question: "The last planet in our solar system is?",
+		answer1: "Pluto",
+		answer2: "Neptune",
+		answer3: "Uranis",
+		answer4: "Jupiter",
+		correctAnswer: 2,
+		correctAnswerNarative: "The last planet in our solar system is Neptune",
+		answerImage: "assets/images/442079-neptune.jpg",
 	}];
 
 //console.log("Correct Answer: " + questions[0].correctAnswer)
@@ -126,7 +126,7 @@ var questionTimer = {
 	},
 
 	duration: function() {
-		if (questionTimer.time < 3) {
+		if (questionTimer.time < 30) {
 			questionTimer.count();
 		}
 
@@ -259,24 +259,31 @@ function correctAnswer(lastChar) {
 
 function holdAnswerTimer() {
 	if (questionsAsked === 8) {
-		setTimeout(gameOver, 1000 * 2);
+		setTimeout(gameOver, 1000 * 5);
 	}
 	else {
-		setTimeout(nextQuestion, 1000 * 1);
+		setTimeout(nextQuestion, 1000 * 5);
 	}
 }
 
 function nextQuestion() {
-	$("#narative").html("");
-	$("#image").attr("src", "");
+	removeNarativeAndImage();
 	unhide();
 	questionTimer.reset();
 	questionLoad();
 }
 
+function removeNarativeAndImage() {
+	var parent0 = document.getElementById("potentialAnswer3-4Row");
+	var child0 = document.getElementById("image");
+	var parent1 = document.getElementById("potentialAnswer1-2Row");
+	var child1 = document.getElementById("narative");
+	parent0.removeChild(child0);
+	parent1.removeChild(child1);
+}
+
 function gameOver() {
-	$("#narative").html("");
-	$("#image").attr("src", "");
+	removeNarativeAndImage();
 
 	var newDiv1 = $("<div></div>");
 	newDiv1.attr("id", "correct");
@@ -298,7 +305,7 @@ function gameOver() {
 	$("#incorrect").text("Incorrect Answers: " + incorrectAnswers);
 	$("#unanswered").text("Unanswered Questions: " + unAnswered);
 
-	setTimeout(restartGame, 1000 * 2);
+	setTimeout(restartGame, 1000 * 5);
 }
 
 function restartGame() {
